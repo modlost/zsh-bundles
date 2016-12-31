f8020997161401c37741d5c261f3578d5080477a
Abner Ballardo <modlost@modlost.net>
2016-12-31 17:18:54 -0500

    fixing names

--- plugins/aws/aws.plugin.zsh
+++ plugins/aws/aws.plugin.zsh
@@ -1,6 +1,6 @@
 export AWS_HOME=~/.aws
 
-function aws-profile-list {
+function aws-profile-active {
   echo $AWS_DEFAULT_PROFILE
 }
 
@@ -12,7 +12,7 @@ function aws-profile-list {
   grep profile ~/.aws/config | sed -e 's/\[profile \(.*\)\]/\1/g'
 }
 
-compctl -K aws-profile-list aws-profile-set aws-profile-list
+compctl -K aws-profile-active aws-profile-set aws-profile-list
 
 completer="/usr/local/aws/bin/aws_zsh_completer.sh"
 [ -x $completer ] && source $completer
